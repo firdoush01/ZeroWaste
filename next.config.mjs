1e6e10858b299eb90c9620ae4cf789bb7eea8c91
firdoush <mdfirdoushali1776@gmail.com>
2024-10-29 16:08:40 +0530

    web3auth+header component

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+import { WEB3AUTH_NETWORK } from '@web3auth/base';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        DATABASE_URL: process.env.DATABASE_URL,
+        WEB3_AUTH_CLIENT_ID: process.env.WEB3_AUTH_CLIENT_ID
+    }
+};
 
 export default nextConfig;
